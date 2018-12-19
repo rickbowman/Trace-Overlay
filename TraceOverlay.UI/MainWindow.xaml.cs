3604b486c466b04f2535fa85d51ee4002728cc23
rickbowman <rickpbowmaniii@gmail.com>
2018-12-18 19:00:41 -0600

    initial commit

---
+++ TraceOverlay.UI/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TraceOverlay.UI
+{
+    public partial class MainWindow : Window
+    {
+        public BitmapImage imagePreview { get; set; }
+        public Bitmap image { get; set; }
+        public TraceWindow tw { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ToSecondaryScreen();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            if (tw != null)
+            {
+                image = null;
+                chkDisplayOverlay.IsChecked = false;
+            }
+
+            Microsoft.Win32.OpenFileDialog op = new Microsoft.Win32.OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imagePreview = new BitmapImage(new Uri(op.FileName));
+                image = new Bitmap(op.FileName);
+                ImageBrush ib = new ImageBrush();
+                ib.ImageSource = imagePreview;
+                splPreview.Background = ib;
+            }
+        }
+
+        private void ToSecondaryScreen()
+        {
+            var secondaryScreen = Screen.AllScreens.FirstOrDefault(s => !s.Primary);
+            if (secondaryScreen != null)
+            {
+                Left = 1912;
+                Top = secondaryScreen.Bounds.Top;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (tw != null)
+                tw.Close();
+        }
+
+        private void chkDisplayOverlay_Checked(object sender, RoutedEventArgs e)
+        {
+            if (image != null)
+            {
+                tw = new TraceWindow(image);
+                tw.Show(); 
+            }
+        }
+
+        private void chkDisplayOverlay_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (tw != null)
+            {
+                tw.Close();
+                tw = null; 
+            }
+        }
+    }
+}
---
+++ TraceOverlay.UI/TraceWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TraceOverlay.UI
+{
+    public partial class TraceWindow : Window
+    {
+        public TraceWindow(Bitmap imageToDisplay)
+        {
+            InitializeComponent();
+            ToPrimaryScreen();
+
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(imageToDisplay.GetHbitmap(),
+                                  IntPtr.Zero,
+                                  Int32Rect.Empty,
+                                  BitmapSizeOptions.FromEmptyOptions());
+            splOverlay.Background = new ImageBrush(bitmapSource);
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+
+        private void ToPrimaryScreen()
+        {
+            var primaryScreen = Screen.AllScreens.FirstOrDefault(s => s.Primary);
+            if (primaryScreen != null)
+            {
+                Left = 0;
+                Top = 0;
+            }
+        }              
+    }
+
+    public static class WindowsServices
+    {
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+}
